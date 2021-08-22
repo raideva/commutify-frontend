7a147952352f12ad9e34aef8ddab85d15113f89d
Tarun-Arora <tarun.arora.cse20@iitbhu.ac.in>
2021-08-22 23:35:40 +0530

    Shifted To Vue2 for Vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
