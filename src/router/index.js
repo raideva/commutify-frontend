a3ae082f5fe7891f23e02993bc612017f8cd5298
pop_os <pop_os@pop-os.localdomain>
2021-10-21 03:43:30 +0530

    Profile Pge

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import ResetPwd from '../views/ResetPwd.vue'
 import store from '../store/index'
 import Main from '../views/Main.vue'
 import Requests from '../views/Requests.vue'
+import Profile from '../views/Profile.vue'
 
 Vue.use(VueRouter)
 
@@ -56,7 +57,16 @@ const routes = [
       if(store.state.auth.token!=null)return next({name: 'Home'})
       next()
     }
-  }
+  },
+  {
+    path: '/profile/:username',
+    name: 'ProfileView',
+    component: Profile,
+    beforeEnter: (to, from, next) => {
+      if(store.state.auth.token==null)return next({name: 'Main'})
+      next()
+    }
+  },
 ]
 
 const router = new VueRouter({
