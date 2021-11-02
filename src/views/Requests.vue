2746245ffdd82723314997b71fbf63470fec0460
Tarun <tarun.arora.cse20@iitbhu.ac.in>
2021-11-02 19:42:39 +0530

    Bug Fixes

--- src/views/Requests.vue
+++ src/views/Requests.vue
@@ -1,126 +1,174 @@
 <template>
-    <div>
-      <Navbar />
-     <v-container>
-          <v-card v-for="item in requests" :key="item.index" class="cards" @click="show(item)">
-            <div class="text-h5" v-if="item.type">
-              <v-card-title> {{ item.name }} </v-card-title>
-              <v-card-text class="text-h7"> Description : <i> {{ item.description }} </i>
-              <v-chip class="tags"  color="red">Group Request</v-chip></v-card-text>
-            </div>
-              <div class="text-h5" v-else>
-              <v-card-title> {{ item.username }} </v-card-title>
-              <v-card-text class="text-h7"> {{ item.first_name }} {{ item.last_name }} 
-              <v-chip class="tags" color="green">Friend Request</v-chip></v-card-text>
-            </div>
-              
-              
-          </v-card>
+  <div>
+    <nav id="navbar">
+      <img src="../assets/Commutify-logos_white.png" alt="" class="logo" />
+    </nav>
+    <v-container class="requests">
+      <v-card
+        v-for="item in requests"
+        :key="item.index"
+        class="cards"
+        @click="show(item)"
+      >
+        <div class="text-h5" v-if="item.type">
+          <v-card-title> {{ item.name }} </v-card-title>
+          <v-card-text class="text-h7">
+            Description : <i> {{ item.description }} </i>
+            <v-chip class="tags" color="red">Group Request</v-chip></v-card-text
+          >
+        </div>
+        <div class="text-h5" v-else>
+          <v-card-title> {{ item.username }} </v-card-title>
+          <v-card-text class="text-h7">
+            {{ item.first_name }} {{ item.last_name }}
+            <v-chip class="tags" color="green"
+              >Friend Request</v-chip
+            ></v-card-text
+          >
+        </div>
+      </v-card>
     </v-container>
-    </div>
+  </div>
 </template>
 
 <script>
-import Navbar from "../components/Navbar.vue";
 import axios from "axios";
 import { mapActions } from "vuex";
 
-export default ({
-    components: { Navbar },
-    data() {
-      return {
+export default {
+  data() {
+    return {
       requests: [],
       requestSocket: null,
-    };    
+    };
+  },
+  methods: {
+    ...mapActions({
+      setToken: "auth/setToken",
+    }),
+    show(a) {
+      var st = a.type === 1 ? a.name : a.username;
+      this.$fire({
+        title: "Accept request from " + st + " ?",
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText: "Accept",
+        cancelButtonText: "Decline",
+      }).then((result) => {
+        if (result.value) {
+          a.type == 1
+            ? this.grpResponse(a.id, 1)
+            : this.frResponse(a.username, 1);
+        } else if (result.dismiss === "cancel") {
+          a.type == 1
+            ? this.grpResponse(a.id, 0)
+            : this.frResponse(a.username, 0);
+        }
+      });
     },
-    methods: {
-        ...mapActions({
-          setToken: "auth/setToken",
-        }),
-        show(a) {
-          var st = (a.type === 1) ? a.name : a.username;
-          this.$fire({
-          title: "Accept request from " + st + " ?",
-          showCloseButton: true,
-          showCancelButton: true,
-          focusConfirm: false,
-          confirmButtonText: 'Accept',
-          cancelButtonText: 'Decline',
-        }).then((result) => {
-          if (result.value) {
-            (a.type == 1) ? this.grpResponse(a.id, 1) : this.frResponse(a.username, 1);}
-          else if (result.dismiss === "cancel"){
-            (a.type == 1) ? this.grpResponse(a.id, 0) : this.frResponse(a.username, 0);}
+    getRequests() {
+      axios({
+        headers: { Authorization: "Token " + this.$store.state.auth.token },
+        url: "api/requests/",
+        method: "get",
+      }).then((res) => {
+        this.requests = res.data;
+      });
+    },
+    grpResponse(id, bool) {
+      axios({
+        headers: { Authorization: "Token " + this.$store.state.auth.token },
+        url: "api/grp_response/",
+        method: "post",
+        data: {
+          id: id,
+          bool: bool,
+        },
+      })
+        .then((res) => {
+          this.getRequests();
+          console.log(res);
         })
+        .catch((e) => console.log(e));
+    },
+
+    frResponse(username, bool) {
+      axios({
+        headers: { Authorization: "Token " + this.$store.state.auth.token },
+        url: "api/fr_response/",
+        method: "post",
+        data: {
+          username: username,
+          bool: bool,
         },
-        getRequests() {
-          axios({
-            headers: { Authorization: "Token " + this.$store.state.auth.token },
-            url: "api/requests/",
-            method: "get",
-          }).then((res) => {
-            this.requests = res.data;
-          });
-        },
-        grpResponse(id, bool) {
-          axios({
-            headers: { Authorization: "Token " + this.$store.state.auth.token },
-            url: "api/grp_response/",
-            method: "post",
-            data: {
-            id: id,
-            bool: bool,
-            },
-          })
-          .then((res) => {
+      })
+        .then((res) => {
           this.getRequests();
           console.log(res);
-          })
-          .catch((e) => console.log(e));
-          },
+        })
+        .catch((e) => console.log(e));
+    },
+    makeConnection() {
+      var self = this;
+      this.requestSocket = new WebSocket(
+        `ws://127.0.0.1:8000/ws/requests/${this.$store.state.auth.token}/`
+      );
 
-        frResponse(username, bool) {
-          axios({
-            headers: { Authorization: "Token " + this.$store.state.auth.token },
-            url: "api/fr_response/",
-            method: "post",
-            data: {
-            username: username,
-            bool: bool,
-            },
-          })
-          .then((res) => {
-          this.getRequests();
-          console.log(res);
-          })
-          .catch((e) => console.log(e));
-        },
-        makeConnection() {
-          var self = this;
-          this.requestSocket = new WebSocket( `ws://127.0.0.1:8000/ws/requests/${this.$store.state.auth.token}/`);
-
-        this.requestSocket.onmessage = function (e) {
+      this.requestSocket.onmessage = function (e) {
         console.log(e.data);
         self.requests.push(JSON.parse(e.data));
-        
       };
 
-          this.requestSocket.onclose = function (e) {console.error("Chat socket closed unexpectedly", e);
-        };
+      this.requestSocket.onclose = function (e) {
+        console.error("Chat socket closed unexpectedly", e);
+      };
     },
-    },
-    created() {
+  },
+  created() {
     this.getRequests();
     this.makeConnection();
-    },
-
-})
+  },
+};
 </script>
 <style lang="scss" scoped>
-  .cards{
-      margin-bottom: 30px;
-  }
-  .tags{
-      float: right;
-  }
+html, body{
+  background-color: black;
+}
+
+#navbar {
+  background-color: black;
+  display: flex;
+  align-items: center;
+  padding: 20px 8%;
+  margin-bottom: 15px;
+}
+
+#navbar .logo {
+  width: 150px;
+  cursor: pointer;
+}
+#navbar ul {
+  flex: 1;
+  text-align: right;
+}
+#navbar ul li {
+  display: inline-block;
+  list-style: none;
+}
+#navbar ul li a {
+  text-decoration: none;
+  color: #fff;
+}
+.cards {
+  margin-bottom: 30px;
+}
+.tags {
+  float: right;
+}
+.requests{
+  height: 100vh;
+  overflow: scroll;
+  padding-bottom: 100px;
+}
 </style>
